03c41d2e6edad8524a65308edb6ae4cb8fb09e4a
SaiKrishna Ogiboyina <C-SaiKrishna.Ogiboyina@charter.com>
2020-02-28 10:34:17 +0530

    adding

---
+++ src/main/resources/info/inet/SampleServlet.java
@@ -0,0 +1,11 @@
+package info.inet;
+
+@WebServlet("/hello")
+public class SampleServlet extends HttpServlet {
+   private static final long serialVersionUID = 1L;
+   @Override
+   protected void doGet(HttpServletRequest req, HttpServletResponse resp) 
+         throws ServletException, IOException {
+      resp.setContentType("text/plain");
+      resp.getWriter().write("Hello World! Maven Web Project Example.");
+   }
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,11 @@
+<html>
+  <head>
+    <title>Your Title Here</title>
+  </head>
+  <body>
+    <form action="/hello" method="POST">
+      <input type="text" id="firstname" name="firstname" />
+      <input type="submit" value="Submit" />
+    </form>
+  </body>
+</html>
