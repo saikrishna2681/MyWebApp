ab473f25ec4c6174357ad4eba9e7b91fe5ec629f
SaiKrishna2681 <45065762+SaiKrishna2681@users.noreply.github.com>
2020-03-03 16:12:33 +0530

    Update SampleServlet.java

--- src/main/resources/info/inet/SampleServlet.java
+++ src/main/resources/info/inet/SampleServlet.java
@@ -23,7 +23,7 @@ public class SampleServlet extends HttpServlet {
          
          Class.forName("com.mysql.cj.jdbc.Driver");  
          Connection con=DriverManager.getConnection(  
-         "jdbc:mysql://mysql1:3306/MyDB?useUnicode=true&characterEncoding=UTF-8&zeroDateTimeBehavior=CONVERT_TO_NULL&serverTimezone=GMT","root","mysql"); 
+         "jdbc:mysql://mysql1:3306/MyDB","root","mysql"); 
          resp.getWriter().write("Establishing Connection : "+con);
          Statement stmt=con.createStatement();  
          resp.getWriter().write("Statement : "+stmt);
