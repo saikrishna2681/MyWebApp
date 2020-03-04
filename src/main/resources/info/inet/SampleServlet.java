aa0ebabe66b19fd7d76b45644b3dceef07f7238c
SaiKrishna2681 <45065762+SaiKrishna2681@users.noreply.github.com>
2020-03-04 09:51:30 +0530

    Update SampleServlet.java

--- src/main/resources/info/inet/SampleServlet.java
+++ src/main/resources/info/inet/SampleServlet.java
@@ -31,7 +31,7 @@ public class SampleServlet extends HttpServlet {
       
       Class.forName("com.mysql.cj.jdbc.Driver");  
       Connection con=DriverManager.getConnection(  
-      "jdbc:mysql://172.17.0.1:3306/hello_java","demo_java","1234"); 
+      "jdbc:mysql://172.17.0.1:3306/hello_java?useSSL=false","demo_java","1234"); 
           
       Statement stmt=con.createStatement();  
       int updates = stmt.executeUpdate("insert into MyDB.emp values(2,'Krishna')");
