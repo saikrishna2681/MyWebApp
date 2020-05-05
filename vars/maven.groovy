9042a03756de99fc4492d9b7f4d48418aad429c6
SaiKrishna2681 <45065762+SaiKrishna2681@users.noreply.github.com>
2020-05-05 18:26:47 +0530

    Update maven.groovy

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -1,9 +1,5 @@
 def call(def args) {
-      
-     // def mvnhome = tool (name: 'mvn', type: 'maven') + '/bin/mvn'
-//	sh('${mvn} mvn args.goal')
-	def mvn(args) {
-    		script.sh "${script.tool 'mvn'}/bin/mvn -o ${args}"
-  	}
-      
+      	def mvnhome = tool (name: 'mvn', type: 'maven') + '/bin/mvn'
+	sh('${mvn} mvn ${args}')
+	
  }
