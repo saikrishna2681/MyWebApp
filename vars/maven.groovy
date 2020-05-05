9a116d3ac877887b27b8a47b758462ce5d3f6803
SaiKrishna2681 <45065762+SaiKrishna2681@users.noreply.github.com>
2020-05-05 16:00:40 +0530

    Update maven.groovy

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -2,6 +2,6 @@ def call(def args) {
       def mvnHome = tool 'mvn'
       //def javaHome = tool 'JDK8'
       withEnv(["PATH+MAVEN=${mvnHome}/bin"]) {
-          sh "${mvnHome}/bin/mvn ${args} package"
+          sh "${mvnHome}/bin/mvn mvn package"
       }
  }
