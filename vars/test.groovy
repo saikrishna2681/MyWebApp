0391dba3838630fb03bb603cc19562e7f0b2a6b2
SaiKrishna2681 <45065762+SaiKrishna2681@users.noreply.github.com>
2020-05-06 14:41:45 +0530

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -5,7 +5,7 @@ def call(String name, String value, String project) {
     sh "docker image build -t 2681/${project} ."
     withCredentials([usernamePassword(credentialsId: 'docker', passwordVariable: 'Sai@2681', usernameVariable: '2681')]) {
 		// some block
-     	sh "docker login -u '$USER' -p '$PASS'"
+     	sh "docker login -u '$USER' -p '$PASSWORD'"
 	
     }
     echo "Hello, ${name}."
