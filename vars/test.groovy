d64145b15c18275260d3457c0b8d6bd009bf499c
SaiKrishna2681 <45065762+SaiKrishna2681@users.noreply.github.com>
2020-05-06 14:34:49 +0530

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,6 +1,8 @@
-def call(String name, String value) {
+def call(String name, String value, String project) {
     // Any valid steps can be called from this code, just like in other
     // Scripted Pipeline
+    echo "Project Name : ${project}"
+	sh "docker image build -t 2681/${project} ."
     echo "Hello, ${name}."
     echo "Hi, ${value}."
 }
