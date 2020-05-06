4bfbbe3fa2e93bb68e96021406713119a21f97fb
SaiKrishna2681 <45065762+SaiKrishna2681@users.noreply.github.com>
2020-05-06 17:54:23 +0530

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,14 +1,21 @@
-def call(String name, String value, String project) {
+def call(String name, String value, String project, String tag) {
     // Any valid steps can be called from this code, just like in other
     // Scripted Pipeline
-    echo "Project Name : ${project}"
-    sh "docker image build -t 2681/${project} ."
-    withCredentials([usernamePassword(credentialsId: 'docker', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+    	echo "Project Name : ${project}"
+	echo "TAG : ${tag}"
+    	//sh "docker image build -t 2681/${project} ."
+   	 //withCredentials([usernamePassword(credentialsId: 'docker', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
 		// some block
-     	sh "docker login -u '$USER' -p '$PASS'"
-	echo "USER $USER "
-	    echo "USER ${PASS}"
-    }
-    echo "Hello, ${name}."
-    echo "Hi, ${value}."
+     		//sh "docker login -u '$USER' -p '$PASS'"
+		//echo "USER $USER "
+	  	//  echo "USER ${PASS}"
+    	//}
+	sh('docker build -t ${project} .')
+	sh('docker tag ${project} saikrishna2681-sai2681.jfrog.io/${project}:${tag}')
+	withCredentials([usernamePassword(credentialsId: 'jfrog', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+    		sh('docker login '$USER' -p '$PASS' saikrishna2681-sai2681.jfrog.io')
+	}
+	sh('docker push saikrishna2681-sai2681.jfrog.io/${project}:${tag}')
+    	echo "Hello, ${name}."
+    	echo "Hi, ${value}."
 }
