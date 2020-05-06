feb3a3567f2a9af5e43a7f524ff750529727fc4a
SaiKrishna2681 <45065762+SaiKrishna2681@users.noreply.github.com>
2020-05-06 14:39:20 +0530

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -2,7 +2,12 @@ def call(String name, String value, String project) {
     // Any valid steps can be called from this code, just like in other
     // Scripted Pipeline
     echo "Project Name : ${project}"
-	sh "docker image build -t 2681/${project} ."
+    sh "docker image build -t 2681/${project} ."
+    withCredentials([usernamePassword(credentialsId: 'docker', passwordVariable: 'Sai@2681', usernameVariable: '2681')]) {
+		// some block
+     	sh "docker login -u '$USER' -p '$PASS'"
+	
+    }
     echo "Hello, ${name}."
     echo "Hi, ${value}."
 }
