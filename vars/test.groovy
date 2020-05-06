8e774aa75cf2f6d040af39a77b3c04e937997525
SaiKrishna2681 <45065762+SaiKrishna2681@users.noreply.github.com>
2020-05-06 14:21:47 +0530

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,5 +1,6 @@
-def call(String name) {
+def call(String name, String value) {
     // Any valid steps can be called from this code, just like in other
     // Scripted Pipeline
     echo "Hello, ${name}."
+    echo "Hi, ${value}."
 }
