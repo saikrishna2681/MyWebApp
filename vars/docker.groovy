6412055aaa3e1c9cb1eb5dad0574cdc6fa741816
SaiKrishna2681 <45065762+SaiKrishna2681@users.noreply.github.com>
2020-05-06 14:11:53 +0530

    Update docker.groovy

--- vars/docker.groovy
+++ vars/docker.groovy
@@ -1,5 +1,5 @@
-def call(String project, String hubUser) {
-	sh "docker image build -t ${hubUser}/${project} ."
+def call(String project) {
+	sh "docker image build -t 2681/${project} ."
 	withCredentials([usernamePassword(credentialsId: 'docker', passwordVariable: 'Sai@2681', usernameVariable: '2681')]) {
 		// some block
 		sh "docker login -u '$USER' -p '$PASS'"
